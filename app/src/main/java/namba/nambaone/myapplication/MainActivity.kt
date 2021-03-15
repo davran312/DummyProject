a14c3e831b59b6710e274e6c5c2a91632e7bb8e6
khasanovdavran10 <davrankhasanov10@gmail.com>
2021-03-15 16:34:25 +0600

    add submodules

--- app/src/main/java/namba/nambaone/myapplication/MainActivity.kt
+++ app/src/main/java/namba/nambaone/myapplication/MainActivity.kt
@@ -2,10 +2,12 @@ package namba.nambaone.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import namba.wallet.nambaone.common.network.okhttp.OkHttpClientFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        OkHttpClientFactory.getInstance(OkHttpClientFactory.Type.SSE)
     }
 }
